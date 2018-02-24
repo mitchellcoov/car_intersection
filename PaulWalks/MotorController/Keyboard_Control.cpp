588c1e4ffcf2a6bca0975fe12fdd317c182e8dea
mitchellcoov <mitchell_coovert@hotmail.com>
2018-02-24 23:32:47 +0000

    Intersection communication to go and notify when clear

--- PaulWalks/MotorController/Keyboard_Control.cpp
+++ PaulWalks/MotorController/Keyboard_Control.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <string>
+#include <sstream>
 #include <termios.h>
 #include <unistd.h>
 #include <sys/types.h> 
@@ -23,6 +25,17 @@
 
 int area[256];
 
+bool recentStop = false;
+
+int listeningPorts[2];
+
+const char *stringListeningPorts;
+
+
+
+int randPort(float irand);
+
+
 /** area**/
 //Prints an error message with the provided message and closes program
 void error(const char *msg);
@@ -33,10 +46,15 @@ void area_retriever(int starting_portno);
 /** intersection**/
 void run_client(void);
 
-void listener(void);
+void listener(int portno);
 
 
 int main() {
+
+    srand(time(NULL));
+
+    std::stringstream ss0;
+    std::stringstream ss1;
 /** area**/
      //Declare variables
      omp_set_nested(1);
@@ -95,20 +113,44 @@ int main() {
 	    currentChannel = ESC_CHANNEL;
 	    currentPWM = PWM_NEUTRAL;
 	    pca9685->setPWM(currentChannel, 0, currentPWM);
+		    listeningPorts[0] = randPort((float)rand());
+		    listeningPorts[1] = randPort((float)rand());
+		    ss0 << listeningPorts[0];
+	
+		    ss1 << listeningPorts[1];
+		
+		    std::string stringPorts = ss0.str() + ":" + ss1.str();
+		    std::cout <<"string ports are " << stringPorts << '\n';
+		    stringListeningPorts = stringPorts.c_str();
+		    printf("char ports are %s", stringListeningPorts);
+
+		    ss0.clear();
+		    ss0.str("");
+		    ss1.clear();
+		    ss1.str("");
 	    #pragma omp parallel num_threads(3)
 	    {
+
+		printf("number of threads: %i\n", omp_get_num_threads());
 		switch(omp_get_thread_num()) {
 		    case 1:
 			printf("listening %i\n", omp_get_thread_num());
-			listener();
+			recentStop = true;
+
+			// Listen for the go ahead
+			listener(listeningPorts[0]);
 			currentChannel = ESC_CHANNEL;
-	    		currentPWM = 344;
+	    		currentPWM = 342;
 			pca9685->setPWM(currentChannel, 0, currentPWM);
+
+			// Listen for cleared intersection
+			listener(listeningPorts[1]);
+			recentStop = false;
+			break;
+
 		    case 2:
 			printf("clienting %i\n", omp_get_thread_num());
 			run_client();
-		    default:
-			printf("defaulting %i\n", omp_get_thread_num());
 			break;
 		}
 	    }
@@ -177,6 +219,7 @@ void error(const char *msg)
 }
 
 void area_retriever(int starting_portno) {
+
      int sockfd, newsockfd, portno;
      socklen_t clilen;
      int buffer[256];
@@ -192,26 +235,29 @@ void area_retriever(int starting_portno) {
      serv_addr.sin_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
+              error("ERROR on binding area_retriever");
 
      fprintf(stdout,"Ready to receive on port %i\n", portno);
      char bufferend[] = "close socket\n";
      while(1){
-          listen(sockfd,5);
-          clilen = sizeof(cli_addr);
-          newsockfd = accept(sockfd, 
+	  if (!recentStop) {
+              listen(sockfd,5);
+              clilen = sizeof(cli_addr);
+              newsockfd = accept(sockfd, 
                       (struct sockaddr *) &cli_addr, 
                       &clilen);
-          if (newsockfd < 0) 
-               error("ERROR on accept");
-//          bzero(buffer,256);
-          n = read(newsockfd,buffer,255);
-	  area[0] = buffer[0];
-          if (n < 0) error("ERROR reading from socket");
+              if (newsockfd < 0) 
+                  error("ERROR on accept");
+//               bzero(buffer,256);
+              n = read(newsockfd,buffer,255);
+              area[0] = buffer[0];
+              if (n < 0) error("ERROR reading from socket");
         //  printf("Here is the message on port %i: %i \n", portno, buffer[0]);
 	  printf("Here is the buffer: %i, and here is the area: %i \n", buffer[0], area[0]);
           n = write(newsockfd,"I got your message",18);
           if (n < 0) error("ERROR writing to socket");
+	  }
+
      }
 
      close(newsockfd);
@@ -225,7 +271,7 @@ void run_client(void) {
 	char buffer[256];
 	char host[14];
 	strcpy(host,"192.168.0.101");
-	
+
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 		error("ERROR opening socket");
@@ -243,7 +289,10 @@ void run_client(void) {
 	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		error("ERROR connecting");
 	bzero(buffer, 256);
-	strcpy(buffer,"here!");
+	
+	printf("char ports are %s", stringListeningPorts);
+	strcpy(buffer, stringListeningPorts);
+	printf("buffer is: %s", buffer); 
 	n = write(sockfd, buffer, strlen(buffer));
 	if (n < 0)
 		error("ERROR writing to socket");
@@ -255,12 +304,13 @@ void run_client(void) {
 	close(sockfd);
 }
 
-void listener(void) {
+void listener(int portno) {
+
+
 	int sockfd, newsockfd, n, stop;
 	socklen_t clilen;
 	char buffer[256];
 	struct sockaddr_in serv_addr, cli_addr;
-	int portno = 21345;
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
@@ -271,10 +321,11 @@ void listener(void) {
 	serv_addr.sin_port = htons(portno);
 	if (bind(sockfd, (struct sockaddr *) &serv_addr,
 		sizeof(serv_addr)) < 0)
-		error("ERROR on binding");
+		error("ERROR on binding listener");
 
 	fprintf(stdout, "Listening on port %i\n", portno);
 	char bufferend[] = "close socket\n";
+
 	listen(sockfd, 5);
 	clilen = sizeof(cli_addr);
 	newsockfd = accept(sockfd,
@@ -286,11 +337,22 @@ void listener(void) {
 	n = read(newsockfd, buffer, 255);
 	if (n < 0) error("ERROR reading from socket");
 	printf("Here is the message: %s \n", buffer);
-	n = write(newsockfd, "Acknowledge", 50);
+	if (recentStop) {
+	    n = write(newsockfd, "Acknowledge GO", 50);
+	} else {
+	    n = write(newsockfd, "Acknowledge CLEAR", 50);
+	}
 	if (n < 0) error("ERROR writing to socket");
+
+
 
 	close(newsockfd);
 	close(sockfd);
+}
+
+int randPort(float irand) {
+    float frand = (irand / RAND_MAX)*(100) + 21300;
+    return (int)frand;
 }
 
 
